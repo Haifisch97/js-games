e719f5370cbf0dac21393d3974a5cfccb3fb003c
Haifisch97 <72409661+Haifisch97@users.noreply.github.com>
2023-03-20 13:02:12 +0200

    mirrored board cells debug

--- js/createBoard.js
+++ js/createBoard.js
@@ -1,7 +1,7 @@
 function createBoard () {
 for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
-        let color = (row + col) % 2 === 0 ? "black" : "white";
+        let color = (row + col) % 2 === 0 ? "white" : "black";
       let cell = `<div class="cell ${color}" data-row="${row}" data-col="${col}"></div>` 
       document.querySelector(".board").innerHTML += cell;
     }
